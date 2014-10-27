14d49c36752435c066799fa8bec9d115d1193b9f
d.popov <d.popov@ancort.ru>
2014-10-27 13:44:18 +0300

    added acra supporting

--- src/com/busylee/audiobook/CAudioBookApplication.java
+++ src/com/busylee/audiobook/CAudioBookApplication.java
@@ -2,10 +2,26 @@ package com.busylee.audiobook;
 
 import android.app.Application;
 import com.busylee.audiobook.base.CSoundTrackStorage;
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
 
 /**
  * Created by busylee on 4/24/14.
  */
+@ReportsCrashes(
+				formKey = "",
+				httpMethod = HttpSender.Method.PUT,
+				reportType = HttpSender.Type.JSON,
+				formUri = "http://62.113.208.110:5984/acra-audio-jobs/_design/acra-storage/_update/report",
+				formUriBasicAuthLogin = "jobs",
+				formUriBasicAuthPassword = "123456",
+				// Your usual ACRA configuration
+				mode = ReportingInteractionMode.TOAST,
+				resToastText = R.string.acra_toast_text
+)
 public class CAudioBookApplication extends Application {
 
     private CSoundTrackStorage mSoundTrackStorage;
@@ -15,6 +31,7 @@ public class CAudioBookApplication extends Application {
     {
         initSingletons();
         super.onCreate();
+		ACRA.init(this);
     }
 
     protected void initSingletons()
